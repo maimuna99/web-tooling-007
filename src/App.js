de4601e2e5b83628a1dcf673aaa69c6840aa9fd6
maimuna99 <73959703+maimuna99@users.noreply.github.com>
2023-02-10 11:48:11 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import Counter from "./counter";
+import Example from "./Requires2";
+const App = () => (
+  <main>
+    {/* <Counter /> */}
+    <Example />
+  </main>
+);
+
+export default App;
