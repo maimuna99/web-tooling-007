b6a994208257c4c59aa13bdb80abafde77dbbde3
Your Name <you@example.com>
2023-02-15 03:19:52 +0100

    storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,10 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/components/**/*.stories.js",
-  ],
-  staticDirs: ["../public"],
+  stories: ["../src/components/**/*.stories.js"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
   ],
   framework: "@storybook/react",
   core: {
